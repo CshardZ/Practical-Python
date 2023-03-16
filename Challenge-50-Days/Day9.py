c8c54fcde760a4e489e512b345c77bb15c063d33
VIvek <108602472+CshardZ@users.noreply.github.com>
2023-03-16 20:08:55 +0530

    Update Day9.py

--- Challenge-50-Days/Day9.py
+++ Challenge-50-Days/Day9.py
@@ -6,9 +6,10 @@ print(biggest_odd('342789')) # 9
 
 # Extra
 def zero_last(llist):
+    llist.sort()
     for i in range(len(llist)):
         if 0 in llist:
             llist.remove(0)
             llist.append(0)
-    return llist.sort()
+    return llist
 print(zero_last([2,4,7,2,6,0,1,0,0,4,5,0])) # [1, 2, 2, 4, 4, 5, 6, 7, 0, 0, 0, 0]
