36d30c385530f2c32c91c78127f59b4dde306b58
VIvek <108602472+CshardZ@users.noreply.github.com>
2023-03-18 13:10:54 +0530

    Update Day10.py

--- Challenge-50-Days/Day10.py
+++ Challenge-50-Days/Day10.py
@@ -2,22 +2,26 @@
 def hide_password(pwd):
     return f"'{len(pwd )*'*'}'"
 
-hide_password('hide')
+print(hide_password('hidden')) # '******'
 
 # Extra
-def seperator(llist):
+nums = [1000000, 2356989, 22354672, 9878098]
+
+def seperator(llist): 🥴
     res = []
     for n in llist:
-        num = str(n)
         nums = []
-        for i in num:
+        for i in str(n):
             nums.append(i)
         for i in range(len(nums)-3,-1,-3):
             nums.insert(i,',')
         
         res.append(''.join(nums))
-    
     return res
 
-nums = [1000000, 2356989, 22354672, 9878098]
 print(seperator(nums)) # ['1,000,000', '2,356,989', '2,354,672', '9,878,098']
+
+def seperator2(llist):
+    nums = [f"{n:,d}" for n in llist]
+    return nums
+print(seperator2(nums)) # ['1,000,000', '2,356,989', '22,354,672', '9,878,098']
